d7e48a85afcb118c3d20496ddd949355b614ada4
quan do <quan.do@asiantech.vn>
2019-10-07 11:40:51 +0700

    setup

---
+++ src/components/layout/InstallBanner.vue
@@ -0,0 +1,42 @@
+<template>
+ <div class="banner" v-if="deferredPrompt">
+   <p>Do you want to install Foo App?</p>
+   <button @click="promptInstall()">Opt for!</button>
+ </div>
+</template>
+
+<script lang="ts">
+ import { Component, Vue } from 'vue-property-decorator';
+ import { BeforeInstallPromptEvent } from 'vue-pwa-install';
+
+ @Component({})
+ export default class InstallBanner extends Vue {
+  public deferredPrompt: BeforeInstallPromptEvent | any;
+
+  public promptInstall() {
+     // Show the prompt:
+     this.deferredPrompt.prompt();
+
+     // Wait for the user to respond to the prompt:
+     this.deferredPrompt.userChoice.then((choiceResult: any) => {
+       if (choiceResult.outcome === 'accepted') {
+         // User accepted the install prompt
+       }
+       this.deferredPrompt = null;
+     });
+   }
+
+  public created() {
+     this.$on('canInstall', (event: BeforeInstallPromptEvent) => {
+       
+       // Prevent Chrome >=67 from automatically showing the prompt:
+       event.preventDefault();
+
+       console.log(event);
+
+       // Stash the event so it can be triggered later:
+       this.deferredPrompt = event;
+     });
+   }
+ }
+</script>
