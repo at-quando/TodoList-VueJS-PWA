a76db9b45020e6f1eb9da351e4d98c19ec465a4b
Phuc Le T.T <phuc.le@asiantech.vn>
2019-10-14 14:27:18 +0700

    code signin

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="todos">
-    <router-view/>
+    <!-- <router-view/> -->
+    <section class="login">
+      <div class="text-center">
+        <section>
+          <img alt="FUA PWA" class="logo" src="@/assets/logo/logo-2x.png">
+          <h1 class="title">Welcome to FUA PWA</h1>
+        </section>
+        <section class="mt-30">
+          <button class="login-btn">Sign up with Google</button>
+          <button class="login-btn" disabled="">Sign up with Guest</button>
+        </section>
+      </div>
+    </section>
   </div>
 </template>
